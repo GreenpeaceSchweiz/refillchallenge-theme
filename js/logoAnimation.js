ea3248aa8f416a8d3bbfaa57eebafe7526e12e64
royas <shekeb.roya@wunderman.ch>
2020-05-27 15:10:25 +0200

    mandatory fields + logo

--- js/logoAnimation.js
+++ js/logoAnimation.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-    var svgTween = new TimelineMax({ delay: 2, repeat: -1 });
+    var svgTween = new TimelineMax({ repeat: -1 });
 
     svgTween.set('.t-mask', { x: 170, y: 200, opacity: 0 });
     svgTween.set('.e-mask', { x: 140, y: 150, opacity: 0 });
@@ -11,15 +11,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     svgTween.set('.d-mask', { x: 120, y: 460, opacity: 0 });
     svgTween.set('.l2-mask', { x: 200, y: 140, opacity: 0 });
 
-    svgTween.to('.t-mask', 1, { y: 140, opacity: 1, ease: "power3. out" })
+    svgTween.to('.t-mask', 1, { delay: 2, y: 140, opacity: 1, ease: "power3. out" })
         .to('.t-mask', 1, { delay: 1, y: 210, opacity: 1, ease: "power3. out" })
-        .to('.e-mask', 1, { delay: 2, y: 50, opacity: 1, ease: "power3. out" })
+        .to('.e-mask', 1, { delay: 2, y: -10, opacity: 1, ease: "power3. out" })
         .to('.e-mask', 1, { delay: 1, y: 150, opacity: 1, ease: "power3. out" })
         .to('.i-mask', 1, { delay: 2, y: 140, opacity: 1, ease: "power3. out" })
         .to('.i-mask', 1, { delay: 1, y: 210, opacity: 1, ease: "power3. out" }) 
         .to('.l-mask', 1, { delay: 2, y: 0, opacity: 1, ease: "power3. out" })
         .to('.l-mask', 1, { delay: 1, y: 110, opacity: 1, ease: "power3. out" })
-        .to('.e2-mask', 1, { delay: 2, y: 105, opacity: 1, ease: "power3. out" })
+        .to('.e2-mask', 1, { delay: 2, y: 50, opacity: 1, ease: "power3. out" })
         .to('.e2-mask', 1, { delay: 1, y: 210, opacity: 1, ease: "power3. out" })
         .to('.r-mask', 1, { delay: 2, y: 50, opacity: 1, ease: "power3. out" })
         .to('.r-mask', 1, { delay: 1, y: 160, opacity: 1, ease: "power3. out" })
